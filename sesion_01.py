1ac76efed030c51bd0550c1d78c423c06cf7e085
ortegadaphne322-a11y <ortegadaphne322@gmail.com>
2026-02-13 16:01:31 -0800

    borre unos gatos

--- sesion_01.py
+++ sesion_01.py
@@ -6,3 +6,24 @@ print(type(7.77))
 print(int(1+2))
 print(int(10*2))
 print("==== Operadores Matematicos========")
+
+
+print(int(2**3))
+print(int(4**8))
+print(float(10%2))
+print(float(25%4))
+
+ventas = 1999999
+print("Nuestras Ventas Fueron: ", ventas)
+
+is_active = True
+print(bool(is_active))
+
+game_over = False
+print(game_over)
+
+edad = 18
+if (edad >= 18):
+    print("si puedo entrar a el Bar!")
+else:
+    print("No puedo entrar en el bar!")
