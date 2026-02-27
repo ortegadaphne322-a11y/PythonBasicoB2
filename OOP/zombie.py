8b70ec10426048ce1c33d62cd932e6e391419e45
daphne <ortegadaphne322@gmail.com>
2026-02-27 15:52:07 -0800

    hice cambios

--- OOP/zombie.py
+++ OOP/zombie.py
@@ -1,10 +1,20 @@
-from Enemigo import *
+from enemigo import *
+import random
 
 class Zombie(Enemigo):
     def __init__(self, puntos_enemigo=10, ataque=1):
-        super().__init__(tipo_enemigo='Zombie', punto_energia=puntos_enemigo, ataque=ataque)
+        super().__init__(tipo_enemigo='Zombie', puntos_energia=puntos_enemigo, ataque=ataque)
         
         def habla(self):
             print("*Hummmm.....*")
+       
         def propagar_enfermedad(self):
             print("El zombie esta tratando de propagar la enfermedad!!")
+         
+         
+        def ataque_especial(self):
+            print("zombie ataque especial")
+            funciona_ataque_especial = random.random() < 0.50
+            if funciona_ataque_especial:
+                self.ataque +=2
+                print("zombie ha generado su energia con 2HP!!!!")
